76a0fc4c7a2678eb64d08bb22edb1f6df7e0239a
wnpfy <ckj8665@gmail.com>
2026-06-23 21:22:22 +0900

    [fix] 언리얼 갈아엎기

--- TrackMaster/Source/TrackMaster/TrackMasterGameMode.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "TrackMasterGameMode.h"
-#include "TrackMasterPlayerController.h"
-
-ATrackMasterGameMode::ATrackMasterGameMode()
-{
-	PlayerControllerClass = ATrackMasterPlayerController::StaticClass();
-}
