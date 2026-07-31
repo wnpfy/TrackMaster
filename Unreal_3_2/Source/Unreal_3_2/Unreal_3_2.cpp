ef030c4b220614cf128543bb4a3be2908858049c
wnpfy <ckj8665@gmail.com>
2026-08-01 01:10:34 +0900

    [fix]깃 이그노어 수정

--- Unreal_3_2/Source/Unreal_3_2/Unreal_3_2.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Unreal_3_2.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Unreal_3_2, "Unreal_3_2" );
